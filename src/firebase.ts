15684f4bec0fd914ad1fa14a0e8eee6d96975a1a
reba1111 <najmadinrebar@gmail.com>
2026-06-25 13:23:41 +0300

    fix: specify default Firestore database ID explicitly

--- src/firebase.ts
+++ src/firebase.ts
@@ -14,5 +14,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app);
+export const db = getFirestore(app, "(default)");
 export const googleProvider = new GoogleAuthProvider();
